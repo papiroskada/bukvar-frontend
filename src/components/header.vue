052721341fefb4da84ff4038963de3c99b20ad1e
dashushak <DunchaSid@gmail.com>
2023-12-06 20:01:24 +0200

    added contacts

--- src/components/header.vue
+++ src/components/header.vue
@@ -17,20 +17,31 @@
         <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <router-link
-                to="/books"
-                class="nav-link active"
-                aria-current="page"
+              <router-link to="/books" class="nav-link active"
                 >Books</router-link
               >
             </li>
             <li class="nav-item">
               <router-link to="/faq" class="nav-link">FAQ</router-link>
             </li>
-            <li class="nav-item">
-              <router-link to="/contacts" class="nav-link"
-                >Contacts</router-link
-              >
+            <li class="nav-item" v-on:click="toggleContactsMenu">
+              <router-link to="/" class="nav-link"
+                >Contacts <i class="fa-solid fa-caret-down"></i
+              ></router-link>
+              <ul v-show="showMenu" class="submenu">
+                <li>
+                  <a href="tel:+380509431722">+38(050)-943-1722</a><br />
+                  <p>manager Daria</p>
+                </li>
+                <li>
+                  <a href="tel:+380959083943">+38(095)-908-3943</a><br />
+                  <p>manager Anton</p>
+                </li>
+                <li>
+                  <a href="mailto:bukVar@gmail.com">bukVar@gmail.com</a><br />
+                  <p>email</p>
+                </li>
+              </ul>
             </li>
           </ul>
           <ul class="navbar-nav mb-2 mb-lg-0 ms-auto">
@@ -43,13 +54,55 @@
     </nav>
   </div>
 </template>
+
 <style>
 .custom-navbar {
   background-color: rgb(193, 136, 80);
 }
+
+.submenu {
+  display: none;
+  position: absolute;
+  background-color: #fff;
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.1);
+  list-style-type: none;
+  padding: 10px 20px;
+  z-index: 9999;
+  border-radius: 3px;
+}
+
+.nav-item:hover .submenu,
+.nav-item.active .submenu {
+  display: block;
+}
+
+.submenu a {
+  color: black;
+}
+
+.submenu a:hover {
+  color: black;
+}
+
+.submenu p {
+  color: rgb(65, 65, 65);
+  font-size: 13px;
+  padding: 0;
+}
 </style>
+
 <script>
 export default {
   name: "HeaderItem",
+  data() {
+    return {
+      showMenu: false,
+    };
+  },
+  methods: {
+    toggleContactsMenu() {
+      this.showMenu = !this.showMenu;
+    },
+  },
 };
 </script>
